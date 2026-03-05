aa7f387fb6e1b082fa1d94b10307a477d1b80118
Harsha Vardhan <149029854+BeyondV0id@users.noreply.github.com>
2026-03-05 17:31:22 +0530

    style: standardize quotation marks in GSOC route file

--- src/routes/GSOC.route.ts
+++ src/routes/GSOC.route.ts
@@ -1,8 +1,8 @@
-import { Router } from 'express';
-import { findGSOC } from '@/controllers/find-gsoc.controller';
+import { Router } from "express";
+import { findGSOC } from "../controllers/find-gsoc.controller";
 
 const router = Router();
 
-router.get('/', findGSOC);
+router.get("/", findGSOC);
 
 export default router;
